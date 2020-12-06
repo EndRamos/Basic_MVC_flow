f49706b3c16a8b9c5df977508bf89730d500bb49
EndRamos <end.cavalcant12@gmail.com>
2020-12-06 15:54:14 -0300

    basic data mvc integration

---
+++ resources/views/client.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cliente</title>
+</head>
+<body>
+    <h1>Clientes</h1>
+    <p>Nome: {{$nome}}</p>
+    <p>E-mail: {{$email}}</p>
+    <p>Telefone: {{$telefone}}</p>
+</body>
+</html>
